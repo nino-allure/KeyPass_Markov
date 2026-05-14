a76cae94e137216c5eba9b3e01dc4a45c2955bbd
DikiYLexa <dubovan36@gmail.com>
2026-05-14 17:18:14 +0500

    Создан UserController Марков

---
+++ KeyPass_Markov/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics.Eventing.Reader;
+using KeyPass_Markov.Classes;
+using KeyPass_Markov.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace KeyPass_Markov.Controllers
+{
+    [Route("/user")]
+    public class UserController : Controller
+    {
+        private DatabaseManager databaseManager;
+        public UserController()
+        {
+            this.databaseManager = this.databaseManager = new DatabaseManager();
+        }
+        [Route("login")]
+        [HttpPost]
+        public ActionResult Login([FromForm] string login, [FromForm] string password)
+        {
+            try
+            {
+                User? AuthUser = databaseManager.Users
+                    .Where(x => x.Login == login && x.Password == password)
+                    .FirstOrDefault();
+                if (AuthUser == null)
+                {
+                    return StatusCode(401);
+                }
+                else
+                {
+                    string Token = JwtToken.Generate(AuthUser);
+                    AuthUser.LastAuth = DateTime.Now;
+                    databaseManager.SaveChanges();
+                    return Ok(new { token = Token });
+                }
+            }
+            catch (Exception exp)
+            {
+                return StatusCode(501, exp.Message);
+            }
+        }
+    }
+}
