6882f8b7dc50e5f95e36d301c4f494cf93ccb9b6
DikiYLexa <dubovan36@gmail.com>
2026-05-14 16:48:25 +0500

    Подключение Swagger для сервера. Марков

--- KeyPass_Markov/Program.cs
+++ KeyPass_Markov/Program.cs
@@ -1,5 +1,29 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMVC(option => option.EnableEndpointRouting = true);
+
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Инструментарий"
+    });
+    string PathFile = Path.Combine(AppContext.BaseDirectory, "KeePass.xml");
+    option.IncludeXmlComments(PathFile);
+});
+
 var app = builder.Build();
+app.UseSwagger();
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Инструментарий");
+});
 
 app.MapGet("/", () => "Hello World!");
 
