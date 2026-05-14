5ec513c5369625ba8e1b7b238beceeee870c45a6
DikiYLexa <dubovan36@gmail.com>
2026-05-14 16:55:39 +0500

    Разработанные модели для сервера.  Markov

---
+++ KeyPass_Markov/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace KeyPass_Markov.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set}
+        public DateTime? LastAuth { get; set; }
+    }
+}
