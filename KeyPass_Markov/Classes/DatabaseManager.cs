7480c200e5c65b835bb3e050b0898f8ce8ef550f
DikiYLexa <dubovan36@gmail.com>
2026-05-14 17:10:46 +0500

    Разработанные вспомогательные классы для сервера. Markov

---
+++ KeyPass_Markov/Classes/DatabaseManager.cs
@@ -0,0 +1,19 @@
+﻿using KeyPass_Markov.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KeyPass_Markov.Classes
+{
+    public class DatabaseManager
+    {
+        public DbSet<Storage> Storages { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DatabaseManager() =>
+            Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=127.0.0.1;uid=student;pwd=;database=Storage;",
+                new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
